c0f4fd003427e33f42626594aadac81ca4e28b75
BilakshanP <pbilakshan@gmail.com>
2026-03-01 16:22:57 +0530

    Added:  feature example

---
+++ aml/examples/styler.rs
@@ -0,0 +1,20 @@
+use aml::styler::Style;
+
+fn main() {
+    let s = Style::new("fR by mbi").unwrap();
+    let c = s.compile();
+
+    println!(
+        "{}",
+        c.style(" Using a pre-computed style. Use me if the style will be used often. ")
+    );
+
+    println!(
+        "{}",
+        Style::apply(
+            "f198 bW mu",
+            " Using on-the-fly style. Use me if the style is used rarely. "
+        )
+        .unwrap()
+    )
+}
