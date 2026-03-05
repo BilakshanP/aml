e37739367f592acb1ddbd5252fc0486a88191527
BilakshanP <pbilakshan@gmail.com>
2026-03-05 22:07:31 +0530

    Update: Improved exports

--- aml/src/lib.rs
+++ aml/src/lib.rs
@@ -1,4 +1,12 @@
-pub use aml_core::*;
+pub use aml_core::parser;
+pub use aml_core::prelude;
+pub use aml_core::render;
+
+#[cfg(feature = "styler")]
+pub use aml_core::styler;
+
+#[cfg(feature = "diagnostics")]
+pub use aml_core::diagnostics;
 
 #[cfg(feature = "macros")]
 pub use aml_macros::style;
